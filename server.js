c21ed780d85f7a359fff98a09e759d0ed50f3d2d
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-09-10 19:30:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+import { app } from "./app.js";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+config();
+
+export const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (err) {
+    console.log("Something went wrong! ", err);
+    process.exit(1);
+  }
+};
+
+dbConnect();
